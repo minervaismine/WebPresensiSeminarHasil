525e9c1b5716b943b34e31aa1f9f53ebddf5a420
minervaismine <romedakarina@gmail.com>
2026-06-06 00:37:35 +0700

    Frontend Page Menu Seminar Saya Mahasiswa Peserta Seminar

--- src/pages/MahasiswaPesertaSeminar_MenuSeminarSaya.jsx
+++ src/pages/MahasiswaPesertaSeminar_MenuSeminarSaya.jsx
@@ -3,12 +3,22 @@ import { Icon } from '@iconify/react';
 
 function MahasiswaPesertaSeminar_MenuSeminarSaya() {
   return (
-    <div className="dashboard-layout">
+    <div className="page-menu-seminar-saya-layout">
         {/* Navbar */}
         <nav className="navbar-menu-seminar-saya">
+            <button className="back-btn">
+                <Icon icon="weui:back-filled" className="back-icon"/>
+                <span>Kembali</span>
+            </button>
 
+            <h1>SEMINAR SAYA</h1>
         </nav>
 
+        <div className="menu-seminar-saya-container">
+            <Icon icon="boxicons:calendar-x-filled" className="no-calendar-icon"/>
+            <h1>Belum ada seminar yang diajukan</h1>
+            <p>Anda belum mengajukan seminar hasil. Pastikan Anda telah memenuhi syarat kehadiran minimal 3 kali sebagai peserta seminar sebelum mengajukan jadwal.</p>
+        </div>
     </div>
   );
 }
