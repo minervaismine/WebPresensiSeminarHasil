4651e01435a8ee4f16a5f57837d3aeb735fe197f
minervaismine <romedakarina@gmail.com>
2026-06-15 21:34:55 +0700

    Menyeseuaikan tampilan NIM di dashboard mahasiswa dengan NIM yang di input

--- frontend/src/pages/mahasiswa/DashboardMahasiswa.jsx
+++ frontend/src/pages/mahasiswa/DashboardMahasiswa.jsx
@@ -1,58 +1,69 @@
 import "../../styles/mahasiswa/DashboardMahasiswa.css";
 import Navbar from "../../components/Navbar";
 import { Icon } from '@iconify/react';
+import { useState, useEffect } from "react";
 
 function Dashboard() {
-  return (
-    <div className="dashboard-layout">
-        <Navbar />
+    const [username, setUsername] = useState("");
 
-        <div className="dashboard-container">
-            {/* Title */}
-            <h1 className="dashboard-title">Selamat Datang, H071221034</h1>
-            <p className="dashboard-description">Aplikasi ini digunakan sebagai presensi online sekaligus memantau kehadiran mahasiswa dalam mengikuti seminar, sebagai syarat mengajukan seminar hasil. Syarat minimal yang harus dipenuhi adalah minimal 3 kali mengikuti seminar.</p>
-        
-            {/* Menu */}
-            <h1 className="menu-title">Menu</h1>
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("user"));
 
-            {/* Card */}
-            <div className="card-container">
-                <div className="menu-card">
-                    <div className="card-header">
-                        <Icon icon="mingcute:clipboard-fill" className="clipboard-icon" />
-                        <span>Seminar Saya</span>
-                    </div>
-                    <div className="card-content">
-                        <p>Lihat informasi seminar Anda dan akses QR Code untuk presensi seminar</p>
-                    </div>
-                    <button className="detail-btn">Lihat Detail</button>
-                </div>
+        if (user) {
+            setUsername(user.username);
+        }
+    }, []);
 
-                <div className="menu-card">
-                    <div className="card-header">
-                        <Icon icon="vaadin:qrcode" className="qrcode-icon" />
-                        <span>Presensi</span>
-                    </div>
-                    <div className="card-content">
-                        <p>Scan QR Code dan validasi lokasi otomatis untuk mencatat kehadiran seminar</p>
-                    </div>
-                    <button className="detail-btn">Lihat Detail</button>
-                </div>
+    return (
+        <div className="dashboard-layout">
+            <Navbar />
 
-                <div className="menu-card">
-                    <div className="card-header">
-                        <Icon icon="tabler:clock-filled" className="history-icon" />
-                        <span>Riwayat Presensi</span>
+            <div className="dashboard-container">
+                {/* Title */}
+                <h1 className="dashboard-title">Selamat Datang, {username}</h1>
+                <p className="dashboard-description">Aplikasi ini digunakan sebagai presensi online sekaligus memantau kehadiran mahasiswa dalam mengikuti seminar, sebagai syarat mengajukan seminar hasil. Syarat minimal yang harus dipenuhi adalah minimal 3 kali mengikuti seminar.</p>
+            
+                {/* Menu */}
+                <h1 className="menu-title">Menu</h1>
+
+                {/* Card */}
+                <div className="card-container">
+                    <div className="menu-card">
+                        <div className="card-header">
+                            <Icon icon="mingcute:clipboard-fill" className="clipboard-icon" />
+                            <span>Seminar Saya</span>
+                        </div>
+                        <div className="card-content">
+                            <p>Lihat informasi seminar Anda dan akses QR Code untuk presensi seminar</p>
+                        </div>
+                        <button className="detail-btn">Lihat Detail</button>
                     </div>
-                    <div className="card-content">
-                        <p>Lihat daftar kehadiran seminar yang telah dihadiri</p>
+
+                    <div className="menu-card">
+                        <div className="card-header">
+                            <Icon icon="vaadin:qrcode" className="qrcode-icon" />
+                            <span>Presensi</span>
+                        </div>
+                        <div className="card-content">
+                            <p>Scan QR Code dan validasi lokasi otomatis untuk mencatat kehadiran seminar</p>
+                        </div>
+                        <button className="detail-btn">Lihat Detail</button>
                     </div>
-                    <button className="detail-btn">Lihat Detail</button>
+
+                    <div className="menu-card">
+                        <div className="card-header">
+                            <Icon icon="tabler:clock-filled" className="history-icon" />
+                            <span>Riwayat Presensi</span>
+                        </div>
+                        <div className="card-content">
+                            <p>Lihat daftar kehadiran seminar yang telah dihadiri</p>
+                        </div>
+                        <button className="detail-btn">Lihat Detail</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Dashboard;
