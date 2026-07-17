27767026caa59c169a526b435a73a68afd710eb6
minervaismine <romedakarina@gmail.com>
2026-07-17 22:45:41 +0700

    Menambahkan marker leaflet

--- frontend/src/pages/admin/KelolaDataLokasi.jsx
+++ frontend/src/pages/admin/KelolaDataLokasi.jsx
@@ -4,6 +4,19 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import api from "../../api/axios";
 import { MapContainer, TileLayer, Marker, useMapEvents, useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
 
 function ChangeMapView({ center }) {
     const map = useMap();
