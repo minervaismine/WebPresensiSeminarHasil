6b99886098cc6cc497f02e43739ccab1745d3c05
minervaismine <romedakarina@gmail.com>
2026-07-23 20:47:41 +0700

    Mencegah data duplikat pada halaman Kelola Data Lokasi

--- frontend/src/pages/admin/KelolaDataLokasi.jsx
+++ frontend/src/pages/admin/KelolaDataLokasi.jsx
@@ -187,17 +187,6 @@ function KelolaDataLokasi() {
             if (err.response && err.response.data) {
                 const { message, field } = err.response.data;
 
-                // Memasukkan pesan error ke bawah input form masing-masing jika diperlukan
-                if (field === "nama_lokasi") {
-                    setErrors(prev => ({ ...prev, nama_lokasi: message }));
-                } else if (field === "koordinat") {
-                    setErrors(prev => ({ 
-                        ...prev, 
-                        latitude: message, 
-                        longitude: message 
-                    }));
-                }
-
                 // Tampilkan alert pemberitahuan duplikasi
                 alert(`Gagal Menambahkan: ${message}`);
             } else {
